0f91b8aad7d3b4c469b0ee64e63fccade893e042
@pierrelstan <stanleypl75@gmail.com>
2019-01-20 17:07:20 -0500

    add some new styles

--- src/App.js
+++ src/App.js
@@ -16,13 +16,20 @@ class App extends Component {
 // arrow fix biding
   handleClickNumbers=(numbers)=>{
   const { display } = this.state;
-  // check if 0 exist in the display if true return an empty state display array
+  // check if 0 exist in the the beginning of display if true return an empty state display array
    if (display[0] === 0){
      this.setState({
-       calculation: [],
+       calculation: [], 
        display:[]
      })
    }
+   if(display.length === 25) {
+     const result = display.filter(num => num.length === 25);
+     this.setState(prevState => ({
+       display: [ result]
+     }));
+   }
+    
     this.setState(prevState => ({
       display: [...prevState.display, numbers]
     }));
@@ -57,34 +64,37 @@ handleClickSigns=(sign)=> {
    return this.Calculation
   }
   render() {
-    const { display, calculation } = this.state;
-    return <div className="App-container">
-        <div id="display"> { display } </div>
-        <button id="clear" onClick={this.ResetButtons}>
-          Clear
-        </button>
-      <button id="equals">
-          =
-        </button>
-        <div className="numbers">
-          {NumbersData.map(data => (
-            <CalculatorNumbers
-              numbers={data.number}
-              key={data.id}
-              id={data.id}
-              onClick={this.handleClickNumbers}
-            />
-          ))}
-        </div>
-        <div className="sign">
-          {SignData.map(dataSign => (
-            <Sign
-              sign={dataSign.sign}
-              key={dataSign.id}
-              id={dataSign.id}
-              onClick={this.handleClickSigns}
-            />
-          ))}
+    const { display } = this.state;
+    return <div className="back-gr">
+      
+        <div className="App-container">
+        <h1 className="title-calculator">calculator</h1>
+          <div id="display"> {display} </div>
+
+          <div className="numbers">
+            {NumbersData.map(data => (
+              <CalculatorNumbers
+                numbers={data.number}
+                key={data.id}
+                id={data.id}
+                onClick={this.handleClickNumbers}
+              />
+            ))}
+            <button id="clear" onClick={this.ResetButtons}>
+              Clear
+            </button>
+            <button id="equals">=</button>
+            <div className="sign">
+              {SignData.map(dataSign => (
+                <Sign
+                  sign={dataSign.sign}
+                  key={dataSign.id}
+                  id={dataSign.id}
+                  onClick={this.handleClickSigns}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>;
   }
