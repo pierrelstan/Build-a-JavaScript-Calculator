f1e6c4f705c335ee5f25434f9c5482518bb80084
@pierrelstan <stanleypl75@gmail.com>
2019-02-05 17:42:12 -0500

    Multiple operators

--- src/App.js
+++ src/App.js
@@ -3,37 +3,37 @@ import './App.css';
 import CalculatorNumbers, { NumbersData } from "./components/numbers";
 import Sign, { SignData } from "./components/sign";
 import Display from './components/display';
-import Dot  from './components/dot';
+import Dot from './components/dot';
 import Results from './components/results';
 
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = { display: [0], newDisplay: [],arrayOne:[], arrayTwo:[], calculation: false, resetButtons: false, sign: [], limitDigit: "limited digits met", num:[], findhowmanydot:true , dotclicked:[], clickDot: [],Dot:[], results:[], result: false };
+    this.state = { display: [0], newDisplay: [], arrayOne: [], arrayTwo: [], calculation: false, resetButtons: false, sign: [], limitDigit: "limited digits met", num: [], findhowmanydot: true, dotclicked: [], clickDot: [], Dot: [], results: [], result: false };
   }
-// arrow fix biding
-  handleClickNumbers=(numbers)=>{
-  const { display, calculation} = this.state;
-  
-  // check if 0 exist in the the beginning of display if true return an empty state display array
-   if (display[0] === 0 ){
-     let displayIndexOne = display[1]
-     this.setState({ display: [displayIndexOne], });
-   }
-   if(display.length === 3){
-    display.filter((data) => {
-      if(data === 0){
-        this.setState({
-          display: []
-        })
-      }
-     })
+  // arrow fix biding
+  handleClickNumbers = (numbers) => {
+    const { display, calculation } = this.state;
 
-     let operators = ["*","-","+","/"];
-     
-   }
-  
+    // check if 0 exist in the the beginning of display if true return an empty state display array
+    if (display[0] === 0) {
+      let displayIndexOne = display[1]
+      this.setState({ display: [displayIndexOne], });
+    }
+    if (display.length === 3) {
+      display.filter((data) => {
+        if (data === 0) {
+          this.setState({
+            display: []
+          })
+        }
+      })
+
+      let operators = ["*", "-", "+", "/"];
+
+    }
+
     if (calculation === true) {
       console.log("this is true")
       this.setState(prevState => ({
@@ -41,181 +41,202 @@ class App extends Component {
         calculation: false
       }))
     }
-    //remove underfined in the begining of the display state array
-      // if (display[0] === "undefined") {
-      //   return this.setState({
-      //     display:display.shift()
-      //   })
 
-      // }
+    if (display.length === 25) {
+      const result = display.filter(num => num.length === 25);
+      this.setState(prevState => ({
+        display: [result],
+      }));
+      const { numbers } = this.state;
+      //  console.log(numbers)
+    }
 
-    // console.log(display)
-   //check if the length of display === 25 
-   if(display.length === 25) {
-     const result = display.filter(num => num.length === 25);
-     this.setState(prevState => ({
-       display: [result],
-     }));
-     const { numbers} = this.state;
-    //  console.log(numbers)
-   }
-    
     this.setState(prevState => ({
       display: [...prevState.display, numbers]
     }));
-    
+
     // console.log(numbers)
-   
+
+
   }
   //that's return  the calculations
-  Calculation=()=> {
-  const { display, arrayOne,  }= this.state;
-    let sign = ["+", "-", "/", "*"];
-    let FindIfAnySignExistAndSelectTheLastOne = sign.map((SignData)=> {
-      return display.filter((displayData)=> {
+  Calculation = () => {
+    const { display, arrayOne, } = this.state;
+    let concatTwoArray = arrayOne.concat(display)
 
-      })
-    })
-    
-    let concatTwoArray = arrayOne.concat(display) 
+    let sign = ["+", "-", "/", "*"];
+    let arr = []
+    let operators = []
+
+
+     concatTwoArray.map((data) => sign.map((signs) => {
+      if (data === signs) {
+        arr.push(data)
+        operators.push(sign)
+      }
+    }))
+
+    // find index one in array 
+    let findIndexSignInNum = concatTwoArray.findIndex((value) => value === operators[0][0])
+    console.log(concatTwoArray + "the index")
+
+    if (arr.length === 2) {
+      
+      let removeArr = arr.toString()
+      let y = concatTwoArray.splice(findIndexSignInNum, arr.length + 1, removeArr)
+      return concatTwoArray
+    }
+    else if (arr.length > 2) {
+      console.log("hello world")
+      
+      let addToNum = concatTwoArray.splice(findIndexSignInNum, arr.length)
+
+    }
+   
+
+
     let turnnumbersToString = concatTwoArray.toString();
-    // let turnToString = results.toString();
+    // let turnToString = display.toString();
     // let replaceItemsTwo = turnToString.replace(/[,]/gi, "");
     let replaceItemsOne = turnnumbersToString.replace(/[,]/gi, "");
 
-  
+
     let total = eval(replaceItemsOne);
-  
+    // let total2 = eval(replaceItemsOne);
+    // console.log(total2)
     this.setState({
       display: [total],
-      // result: [totalresult],
+      // arrayTwo: [total2],
       calculation: true,
     })
     console.log(total + "from calcution")
     // console.log(totalresult + "from calcution")
-      }
 
-handleClickSigns=(sign)=> {
-  this.setState((prevState) => ({
-    display: [...prevState.display,sign],
-    sign:[sign]
-  }))
-// console.log(sign + "from handleClickSigns")
-}
-// Arrow fix biding 
-  ResetButtons = ()=> {
-    const { resetButtons} = this.state;
-    if(!resetButtons){
+  }
+
+
+  handleClickSigns = (sign) => {
+    this.setState((prevState) => ({
+      display: [...prevState.display, sign],
+      sign: [sign]
+    }))
+    // console.log(sign + "from handleClickSigns")
+  }
+  // Arrow fix biding 
+  ResetButtons = () => {
+    const { resetButtons } = this.state;
+    if (!resetButtons) {
       this.setState({
-        display:[0],
-       arrayOne:[]
+        display: [0],
+        arrayOne: []
       })
     }
   }
   //Arrow fix biding 
-  EqualCalculations=()=> {
-   return this.Calculation
+  EqualCalculations = () => {
+    return this.Calculation
   }
   //arrow fix bidng
-handleDot=(dot)=> {
-  const { display, Dot }=  this.state;
-this.setState((prevState=>({
-  display: [...prevState.display, dot],
-  // Dot:[...prevState.Dot,...dot]
+  handleDot = (dot) => {
+    const { display, Dot } = this.state;
+    this.setState((prevState => ({
+      display: [...prevState.display, dot],
+      // Dot:[...prevState.Dot,...dot]
+    })))
+    // console.log(Dot)
+    // search the  quantity of value  "." in display
 
-})))
-// console.log(Dot)
-  // search the  quantity of value  "." in display
-
-  // let displayCopyOne = display.slice(0);
-  let searchPositionOfValueOfDot = display.filter((data)=> {
-    return data === "."
-  })
-  // search the positition on "." in dipslay
-  let searchThePositionOfDot = display.indexOf(".")
+    // let displayCopyOne = display.slice(0);
+    let searchPositionOfValueOfDot = display.filter((data) => {
+      return data === "."
+    })
+    // search the positition on "." in dipslay
+    let searchThePositionOfDot = display.indexOf(".")
     // console.log(searchThePositionOfDot)
-  // the next index of dot in the display state 
-   let theNextValueOfDot = searchThePositionOfDot + 1;
+    // the next index of dot in the display state 
+    let theNextValueOfDot = searchThePositionOfDot + 1;
 
 
-  let theLengthOfDot = searchPositionOfValueOfDot.length;
-  // create a copy of display state to create a new array
-  // create a copy of display to not modify the original array
-  const {calculation } = this.state;
+    let theLengthOfDot = searchPositionOfValueOfDot.length;
+    // create a copy of display state to create a new array
+    // create a copy of display to not modify the original array
+    const { calculation } = this.state;
 
-   let copyArray = display.slice(searchThePositionOfDot + 1)
+    let copyArray = display.slice(searchThePositionOfDot + 1)
 
-  //  find index
-  let Position = copyArray.findIndex((value)=> value === ".");
+    //  find index
+    let Position = copyArray.findIndex((value) => value === ".");
 
-  // lenghtOFNewDot
-  let compareIfDotExist = copyArray.filter((data)=> {
-    return data !== "."
-  }) 
-console.log(compareIfDotExist + " lalal")
-  
+    // lenghtOFNewDot
+    let compareIfDotExist = copyArray.filter((data) => {
+      return data !== "."
+    })
+    console.log(compareIfDotExist + " lalal")
 
-  
- 
 
-if (display[searchPositionOfValueOfDot] === display[theNextValueOfDot]) {
 
- display.splice(theNextValueOfDot - theLengthOfDot, theLengthOfDot + theLengthOfDot) 
-}
-else if (display[searchPositionOfValueOfDot] === display[theNextValueOfDot + 1]) {
 
-let displayOne =display.splice(display[theNextValueOfDot  + 1], theLengthOfDot + theLengthOfDot  + 1  - 2)
 
-console.log(displayOne+ "the first display")
-  this.setState((prevState)=> ({
-    display: [display],
-    arrayOne: [displayOne]
-  }))
-  
+    if (display[searchPositionOfValueOfDot] === display[theNextValueOfDot]) {
 
-} else {
- return
-}  
-}
+      display.splice(theNextValueOfDot - theLengthOfDot, theLengthOfDot + theLengthOfDot)
+    }
+    else if (display[searchPositionOfValueOfDot] === display[theNextValueOfDot + 1]) {
 
- 
+      let displayOne = display.splice(display[theNextValueOfDot + 1], theLengthOfDot + theLengthOfDot + 1 - 2)
+
+      console.log(displayOne + "the first display")
+      this.setState((prevState) => ({
+        display: [display],
+        arrayOne: [displayOne]
+      }))
+
+
+    } else {
+      return
+    }
+
+
+  }
+
+
 
   render() {
     const { display, result } = this.state;
-    return ( <div className="back-gr">
-        <div className="App-container">
-          <h1 className="title-calculator">calculator</h1>
-         <Display display={display} result={result} /> 
-          <div className="numbers">
-            {NumbersData.map(data => (
-              <CalculatorNumbers
-                numbers={data.number}
-                key={data.id}
-                id={data.id}
-                onClick={this.handleClickNumbers}
-              />
-            ))}
-            <button id="clear" onClick={this.ResetButtons}>
-              Clear
+    return (<div className="back-gr">
+      <div className="App-container">
+        <h1 className="title-calculator">calculator</h1>
+        <Display display={display} result={result} />
+        <div className="numbers">
+          {NumbersData.map(data => (
+            <CalculatorNumbers
+              numbers={data.number}
+              key={data.id}
+              id={data.id}
+              onClick={this.handleClickNumbers}
+            />
+          ))}
+          <button id="clear" onClick={this.ResetButtons}>
+            Clear
             </button>
-            <button id="equals" onClick={this.Calculation}>
-              =
+          <button id="equals" onClick={this.Calculation}>
+            =
             </button>
           <Dot dot={"."} onClick={this.handleDot} />
-           
-            <div className="sign">
-              {SignData.map(dataSign => (
-                <Sign
-                  sign={dataSign.sign}
-                  key={dataSign.id}
-                  id={dataSign.id}
-                  onClick={this.handleClickSigns}
-                />
-              ))}
-            </div>
+
+          <div className="sign">
+            {SignData.map(dataSign => (
+              <Sign
+                sign={dataSign.sign}
+                key={dataSign.id}
+                id={dataSign.id}
+                onClick={this.handleClickSigns}
+              />
+            ))}
           </div>
         </div>
-    </div> );
+      </div>
+    </div>);
   }
 }
 
